4afecf74920440a760aaf6feb8a964347b8220c5
Geraint Edwards <geraint.edwards@ext.ons.gov.uk>
2024-03-22 12:01:16 +0000

    add version 'make check' for 'yq'

--- v2/stacks/common.mk
+++ v2/stacks/common.mk
@@ -150,8 +150,9 @@ check-versions:
 	@source $(SCRIPTS_DIR)/utils.sh;	\
 		is_ver java		"$$(java -version		2>&1 | sed -En 's/.* version "(.*)"$$/\1/p')"		"1\.8\.*";		\
 		is_ver maven		"$$(mvn --version		2>&1 | sed -En 's/.* Maven ([0-9]+\..*) .*/\1/p')"	"3\.*";			\
-		is_ver docker		"$$(docker --version		2>&1 | sed -En 's/.* version ([^ ]+), .*/\1/p')"		"25\.*";		\
+		is_ver docker		"$$(docker --version		2>&1 | sed -En 's/.* version ([^ ]+), .*/\1/p')"	"25\.*";		\
 		is_ver docker-compose	"$$(docker-compose --version	2>&1 | sed -En 's/.* version v?([0-9.]+.*)/\1/p')"	"2\.2?\.*";		\
+		is_ver yq		"$$(yq --version		2>&1 | sed -En 's/.* version v?([0-9.]+.*)/\1/p')"	"4\.[4-9][0-9].*";		\
 		: is_ver nvm		"$$(nvm --version		2>&1 )"							"0\.[3-9][0-9]\..*";	\
 		: is_ver npm		"$$(npm --version		2>&1 )"							"0\.[3-9][0-9]\..*"
 
