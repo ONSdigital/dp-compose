146e121bed5d2b8a4e60a078b74dc794cd2c0127
Geraint Edwards <geraint.edwards@ext.ons.gov.uk>
2024-10-11 11:38:12 +0100

    making the cmd stack more likely to run

--- v2/provisioning/localstack/s3.sh
+++ v2/provisioning/localstack/s3.sh
@@ -5,4 +5,8 @@ awslocal s3 mb s3://testing
 awslocal s3 mb s3://testing-public
 awslocal s3 mb s3://dp-frontend-florence-file-uploads
 awslocal s3api put-object --bucket testing --key index.html --body /root/index.html
+
+# dataset-exporter dataset-exporter-xlsx
+awslocal s3 mb s3://csv-exported
+
 set +x
