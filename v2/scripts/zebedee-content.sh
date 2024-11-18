f6aada8bec9c893985ccfb332e71a776b503efc1
janderson2 <janderson2@users.noreply.github.com>
2024-11-18 18:16:03 +0000

    Make clone more generic and move to utils
    Move the github clone logic out to utils package and split out the
    github connection test so it can be common across the clone.sh and
    zebedee-content.sh scripts.

--- v2/scripts/zebedee-content.sh
+++ v2/scripts/zebedee-content.sh
@@ -3,66 +3,21 @@
 DP_COMPOSE_V2_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 source "${DP_COMPOSE_V2_DIR}/scripts/utils.sh"
 
-REPO_DIR="$( cd "${DP_COMPOSE_V2_DIR}/../.." && pwd )"
-
 # Set VERBOSE=true to debug connection issues
 VERBOSE=${VERBOSE:-false}
 
 #==| CONSTANTS |================================================
 
-REPO=dp-zebedee-content
-REPO_URL=https://github.com/ONSdigital/$REPO
-REPO_CLONE_URL=git@github.com:ONSdigital/$REPO
+ZEBEDEE_CONTENT_REPO=dp-zebedee-content
 
 #==| FUNCTIONS |================================================
 
-clone_repo() {
-    # Test ssh to github
-    res=0
-    if [[ $VERBOSE = true ]]; then
-        ssh -v -T git@github.com || res=$?
-    else
-        ssh -T git@github.com &>/dev/null || res=$?
+init_zebedee_content() {
+    local repo_path="$1"
+    if [[ ! -d "${repo_path}" ]]; then
+        fatal "unexpected error: repo directory does not exist: $repo_path"
     fi
-    case $res in
-        1)
-            info "github ssh authentication successful"
-            ;;
-        255)
-            fatal "github ssh authentication failed, ensure ssh is configured for github in order to use this script"
-            ;;
-        *)
-            fatal "github ssh authentication encountered unexpected error"
-    esac
 
-    local repo_pp="$(colour $CYAN $REPO)"
-
-    # Check if the repo already exists locally
-    local repo_path="$REPO_DIR/$REPO"
-    if [[ -d "${repo_path}" ]]; then
-        local branch=$(git -C "$repo_path" rev-parse --abbrev-ref HEAD)
-        local br_colour=$GREEN; [[ $branch == main ]] || br_colour=$YELLOW
-        local branch_pp=$(colour $br_colour $branch)
-        info "repo already cloned, skipping: $repo_pp ($branch_pp)"
-    else
-        # If not then clone it
-        info "cloning repo...: $repo_pp ($REPO_URL)"
-
-        if [[ $VERBOSE = true ]]; then
-            git -C "${REPO_DIR}" clone "$REPO_CLONE_URL"
-        else
-            git -C "${REPO_DIR}" clone "$REPO_CLONE_URL" 2> /dev/null
-        fi
-        if [[ $? > 0 ]]; then
-            fatal "failed to clone repo, please make sure you have access to it: $repo_pp ($REPO_URL)"
-        else
-            info "successfully cloned repo: $repo_pp ($REPO_URL)"
-        fi
-    fi
-}
-
-init_content() {
-    local repo_path="$REPO_DIR/$REPO"
     # This is a very minimal check and does not include a check to insure that the latest content has been
     # initialised after a git pull. This has been done to prevent overwriting local test data unexpected, but
     # may be worth revisiting in the future.
@@ -84,5 +39,6 @@ init_content() {
 [[ -n $BASH_VERSINFO && $BASH_VERSINFO -lt 4 ]] && fatal "your 'bash' is too old, please run 'brew install bash'"
 which git > /dev/null || fatal "git not installed"
 
-clone_repo
-init_content
+test_github_ssh
+clone_repo "$ZEBEDEE_CONTENT_REPO" main
+init_zebedee_content "$DP_REPO_DIR/$ZEBEDEE_CONTENT_REPO"
