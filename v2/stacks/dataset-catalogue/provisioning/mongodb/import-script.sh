a03cfc06195bd6429eee2c7a66688177cbc33da9
BarathaAberathne <barathaaberathne@gmail.com>
2025-03-07 09:23:10 +0000

    added  dataset and version record into datasets db in dataset-catalogue

--- v2/stacks/dataset-catalogue/provisioning/mongodb/import-script.sh
+++ v2/stacks/dataset-catalogue/provisioning/mongodb/import-script.sh
@@ -4,3 +4,4 @@ mongoimport --host 127.0.0.1:27017 --db datasets --collection datasets --file /d
 mongoimport --host 127.0.0.1:27017 --db datasets --collection editions --file /docker-entrypoint-initdb.d/editions.json
 mongoimport --host 127.0.0.1:27017 --db datasets --collection instances --file /docker-entrypoint-initdb.d/instances.json
 mongoimport --host 127.0.0.1:27017 --db datasets --collection dimension.options --file /docker-entrypoint-initdb.d/dimension.options.json
+mongoimport --host 127.0.0.1:27017 --db datasets --collection versions --file /docker-entrypoint-initdb.d/versions.json
