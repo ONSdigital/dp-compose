faf8cb44434b9234271fcd4967ae07b4734a3311
aryan-patel <aryanpatel1771@gmail.com>
2025-03-31 11:52:22 +0100

    added indexes to all collections

--- v2/stacks/dataset-catalogue/provisioning/mongodb/init.js
+++ v2/stacks/dataset-catalogue/provisioning/mongodb/init.js
@@ -2,60 +2,203 @@ var databases = [
 	{
 		name: "datasets",
 		collections: [
-			{"name": "datasets"}, {"name":"contacts"}, {"name":"editions"}, {"name":"instances"}, {"name":"versions"}, {"name":"dimension.options"}, {"name":"instances_locks"}
+			{
+				name: "datasets",
+				index: [
+					{ condition: '{"current.is_based_on.id": 1}', options: '{}' },
+					{ condition: '{"next.is_based_on.id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "editions",
+				index: [
+					{ condition: '{"current.links.dataset.id": 1, "current.edition": 1, "current.state": 1}', options: '{}' },
+					{ condition: '{"next.links.dataset.id": 1, "next.edition": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "instances",
+				index: [
+					{ condition: '{"links.dataset.id": 1, "edition": 1, "version": 1, "state": 1}', options: '{}' },
+					{ condition: '{"state": 1}', options: '{}' },
+					{ condition: '{"id": 1, "import.tasks.build_hierarchies.dimension_name": 1}', options: '{}' },
+					{ condition: '{"id": 1, "import.tasks.build_search_indices.dimension_name": 1}', options: '{}' }
+				]
+			},
+			{ name: "instances_locks" },
+			{
+				name: "versions",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "dimension.options",
+				index: [
+					{ condition: '{"instance_id": 1, "name": 1, "option": 1}', options: '{}' }
+				]
+			},
+			{ name: "contacts" }
 		]
 	},
 	{
 		name: "recipes",
-		collections: [{"name":"recipes"}]
+		collections: [{ name: "recipes" }]
 	},
 	{
 		name: "filters",
-		collections: [{"name":"filters"}, {"name":"filterOutputs"}]
+		collections: [
+			{
+				name: "filters",
+				index: [
+					{ condition: '{"filter_id": 1}', options: '{}' },
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "filterOutputs",
+				index: [
+					{ condition: '{"filter_id": 1}', options: '{}' },
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			}
+		]
 	},
 	{
 		name: "imports",
-		collections: [{"name":"imports"}, {"name":"imports_locks"}]
+		collections: [
+			{
+				name: "imports",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' },
+					{ condition: '{"state": 1, "id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "imports_locks",
+				index: [
+					{ condition: '{"resource": 1}', options: '{"unique": true}' },
+					{ condition: '{"exclusive.LockId": 1}', options: '{}' },
+					{ condition: '{"exclusive.ExpiresAt": 1}', options: '{}' },
+					{ condition: '{"shared.locks.LockId": 1}', options: '{}' },
+					{ condition: '{"shared.locks.ExpiresAt": 1}', options: '{}' }
+				]
+			}
+		]
 	},
 	{
 		name: "topics",
-		collections: [{"name":"topics"}, {"name":"content"}]
+		collections: [
+			{
+				name: "topics",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "content",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			}
+		]
 	},
 	{
 		name: "search",
-		collections: [{"name":"jobs"}, {"name":"jobs_locks"}]
+		collections: [
+			{
+				name: "jobs",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "jobs_locks",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' },
+					{ condition: '{"resource": 1}', options: '{}' },
+					{ condition: '{"exclusive.LockId": 1}', options: '{}' },
+					{ condition: '{"exclusive.ExpiresAt": 1}', options: '{}' },
+					{ condition: '{"shared.locks.LockId": 1}', options: '{}' },
+					{ condition: '{"shared.locks.ExpiresAt": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "tasks",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			}
+		]
 	},
 	{
 		name: "images",
-		collections: [{"name":"images"}, {"name":"images_locks"}]
+		collections: [
+			{
+				name: "images",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' },
+					{ condition: '{"collection_id": 1}', options: '{}' }
+				]
+			},
+			{
+				name: "images_locks",
+				index: [
+					{ condition: '{"resource": 1}', options: '{}' },
+					{ condition: '{"exclusive.LockId": 1}', options: '{}' },
+					{ condition: '{"exclusive.ExpiresAt": 1}', options: '{}' },
+					{ condition: '{"shared.locks.LockId": 1}', options: '{}' },
+					{ condition: '{"shared.locks.ExpiresAt": 1}', options: '{}' }
+				]
+			}
+		]
 	},
 	{
 		name: "files",
-		collections: [{"name":"metadata"},{"name":"collections"},{"name":"bundles"}]
+		collections: [
+			{
+				name: "metadata",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' },
+					{ condition: '{"collection_id": 1}', options: '{}' },
+					{ condition: '{"path": 1}', options: '{"unique": true}' }
+				]
+			},
+			{
+				name: "collections",
+				index: [
+					{ condition: '{"id": 1}', options: '{"unique": true}' }
+				]
+			},
+			{ name: "bundles" }
+		]
 	},
 	{
-        name: "permissions",
-        collections: [{"name":"roles"}, {"name":"policies"}]
-	},
-	{
-		name: "search",
-		collections: [{"name":"jobs"}, {"name":"jobs_locks"}, {"name":"tasks"}]
+		name: "permissions",
+		collections: [
+			{
+				name: "roles",
+				index: [
+					{ condition: '{"id": 1}', options: '{}' }
+				]
+			},
+			{ name: "policies" }
+		]
 	}
 ];
 
 for (database of databases) {
-	db = db.getSiblingDB(database.name);
+    db = db.getSiblingDB(database.name);
 
-	for (collection of database.collections){
-		db.createCollection(collection["name"]);
-	
-		if (collection["index"]) {
-	
-			for (index of collection["index"]){
+    for (collection of database.collections){
+        db.createCollection(collection["name"]);
+    
+        if (collection["index"]) {
+            for (index of collection["index"]){
 				printjson(index)
-				results = db[collection["name"]].createIndex(JSON.parse(index["condition"]), JSON.parse(index["options"]))
+                results = db[collection["name"]].createIndex(JSON.parse(index["condition"]), JSON.parse(index["options"]))
 				printjson(results);
-			}
+            }
         }
-	}
+    }
 }
