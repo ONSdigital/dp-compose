f5f434fa51965847e55c3905c01723f17b9afa79
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-04-02 13:22:24 +0100

    Add localstack s3 upload bucket

--- v2/provisioning/localstack/s3.sh
+++ v2/provisioning/localstack/s3.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -x
 awslocal s3 mb s3://deprecated
-awslocal s3 mb s3://testing
 awslocal s3 mb s3://testing-public
 awslocal s3 mb s3://dp-frontend-florence-file-uploads
 awslocal s3api put-object --bucket testing --key index.html --body /root/index.html
 
 # dataset-exporter dataset-exporter-xlsx
 awslocal s3 mb s3://csv-exported
+
+# dp-image importer
+awslocal s3 mb s3://testing
+awslocal s3 mb s3://uploaded
 
 set +x
