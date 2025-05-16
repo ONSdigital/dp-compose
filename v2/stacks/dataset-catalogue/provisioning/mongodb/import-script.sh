c9cf83b4a544f75eafbaf4a55f27aa80d879e846
aryan-patel <aryanpatel1771@gmail.com>
2025-05-16 08:32:43 +0100

    add test data for bundle-contents collection

--- v2/stacks/dataset-catalogue/provisioning/mongodb/import-script.sh
+++ v2/stacks/dataset-catalogue/provisioning/mongodb/import-script.sh
@@ -7,3 +7,4 @@ mongoimport --host 127.0.0.1:27017 --db datasets --collection dimension.options 
 mongoimport --host 127.0.0.1:27017 --db datasets --collection versions --file /docker-entrypoint-initdb.d/versions.json
 mongoimport --host 127.0.0.1:27017 --db bundles --collection bundles --file /docker-entrypoint-initdb.d/bundles.json
 mongoimport --host 127.0.0.1:27017 --db bundles --collection bundle_events --file /docker-entrypoint-initdb.d/bundle-events.json
+mongoimport --host 127.0.0.1:27017 --db bundles --collection bundle_contents --file /docker-entrypoint-initdb.d/bundle-contents.json
