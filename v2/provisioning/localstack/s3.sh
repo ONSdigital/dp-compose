d86de7326bf099a9c3366f29f4350eb4898c0386
fran moore <franmoore05@gmail.com>
2025-09-09 14:34:18 +0100

    Adding ingest-datasets bucket

--- v2/provisioning/localstack/s3.sh
+++ v2/provisioning/localstack/s3.sh
@@ -5,6 +5,7 @@ awslocal s3 mb s3://testing-public
 awslocal s3 mb s3://dp-frontend-florence-file-uploads
 awslocal s3 mb s3://uploaded
 awslocal s3 mb s3://testing
+awslocal s3 mb s3://ingest-datasets
 awslocal s3api put-object --bucket testing --key index.html --body /root/index.html
 
 # dataset-exporter dataset-exporter-xlsx
